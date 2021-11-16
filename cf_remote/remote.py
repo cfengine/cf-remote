cfengine/cf-remote

commit e61ed477e42d1a465d8d11d6faef30df049d23d2

Add support for local operations when targeting localhost
We modify the behaviour of `cf-remote` when the target host is
localhost: in this case the actions executed on the local host without
connecting over SSH.

Signed-off-by: Fabio Tranchitella <fabio.tranchitella@northern.tech>

cf_remote/remote.py
@@ -309,10 +309,10 @@ def install_host(
         r = ssh_cmd(cmd="curl --fail -O {}".format(package), connection=connection, errors=True)
         if r is None:
             return 1
-    else:
+    elif not connection.is_local:
         scp(package, host, connection=connection)
+        package = basename(package)
 
-    package = basename(package)
     install_package(host, package, data, connection=connection)
     data = get_info(host, connection=connection)
     if data["agent_version"] and len(data["agent_version"]) > 0:
@@ -387,8 +387,10 @@ def deploy_masterfiles(host, tarball, *, connection=None):
         log.error(f"Cannot deploy masterfiles on {host} - CFEngine not installed")
         return 1
     
-    scp(tarball, host, connection=connection, rename="masterfiles.tgz")
-    ssh_cmd(connection, f"tar -xzf masterfiles.tgz")
+    if not connection.is_local:
+        scp(tarball, host, connection=connection, rename="masterfiles.tgz")
+        tarball = "masterfiles.tgz"
+    ssh_cmd(connection, f"tar -xzf {tarball}")
     commands = [
         "systemctl stop cfengine3",
         "rm -rf /var/cfengine/masterfiles",

cf_remote/commands.py
@@ -66,6 +66,7 @@ def scp(hosts, files, users=None):
             errors += transfer_file(host, file, users)
     return errors
 
+
 def _download_urls(urls):
     """Download packages from URLs, replace URLs with filenames
 

cf_remote/ssh.py
@@ -1,11 +1,33 @@
 import os
 import sys
 import fabric
+import pwd
+import shutil
+import subprocess
+
 from paramiko.ssh_exception import AuthenticationException
 from invoke.exceptions import UnexpectedExit
 
 from cf_remote import log
 from cf_remote.utils import whoami
+
+
+class LocalConnection:
+    is_local = True
+    ssh_user = None
+    ssh_host = "localhost"
+
+    def __init__(self):
+        self.ssh_user = pwd.getpwuid(os.getuid()).pw_name
+
+    def run(self, command, hide=False, pty=False):
+        return subprocess.run(command, capture_output=True, shell=True, text=True)
+
+    def put(self, src):
+        src = os.path.abspath(src)
+        dst = os.path.basename(src)
+        if src != dst:
+            shutil.copy(src, dst)
 
 
 def connect(host, users=None):
@@ -55,6 +77,9 @@ def connect(host, users=None):
 def auto_connect(func):
     def connect_wrapper(host, *args, **kwargs):
         if not kwargs.get("connection"):
+            if host == "localhost":
+                kwargs["connection"] = LocalConnection()
+                return func(host, *args, **kwargs)
             with connect(host, users=kwargs.get("users")) as connection:
                 assert connection
                 kwargs["connection"] = connection

cf_remote/utils.py
@@ -28,7 +28,7 @@ def canonify(string):
 
 
 def user_error(msg):
-    sys.exit("cf-remote: " + msg)
+    sys.exit(f"{os.path.basename(sys.argv[0])}: " + msg)
 
 
 def exit_success():

commit 82421c17385bf6d6b37d35171775856ebb554bdf

Fix --remote-download to install the downloaded package
This was broken in e61ed477e4. The install_package() function
needs to be called with the downloaded package, not its URL.

Ticket: CFE-3869
Changelog: None

cf_remote/remote.py
@@ -309,6 +309,7 @@ def install_host(
         r = ssh_cmd(cmd="curl --fail -O {}".format(package), connection=connection, errors=True)
         if r is None:
             return 1
+        package = basename(package)
     elif not getattr(connection, 'is_local', False):
         scp(package, host, connection=connection)
         package = basename(package)
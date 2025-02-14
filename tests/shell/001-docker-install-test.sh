244fac422f75aa43beed274d4e7352503d0474db
Craig Comstock <craig.comstock@northern.tech>
2025-02-14 11:54:48 -0600

    test

--- tests/shell/001-docker-install-test.sh
+++ tests/shell/001-docker-install-test.sh
@@ -16,6 +16,6 @@ docker run -d -p 8822:22 --name "$name" "$name" >>log 2>&1
 ip_addr=$(hostname -i)
 ssh -o StrictHostKeyChecking=no -p 8822 root@"$ip_addr" hostname >>log 2>&1
 echo "ssh returned exit code $?"
-false
+echo "=== cf-remote install ===" | tee -a log
 cf-remote --log-level DEBUG install --clients root@"$ip_addr":8822 2>&1 | tee -a log
 ssh -o StrictHostKeyChecking=no -p 8822 root@"$ip_addr" cf-agent -V >>log 2>&1

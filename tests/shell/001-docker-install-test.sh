51348a14e6950f1100478090eeb99b04867393c3
Craig Comstock <craig.comstock@northern.tech>
2025-02-14 10:55:41 -0600

    Added cf-remote install tests in a docker container and on test runner machine
    These were added to expose bug in how nt-discovery.sh is managed and used.
    
    Ticket: ENT-12668

---
+++ tests/shell/001-docker-install-test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -ex
+error () {
+  echo "=== an error occurred, logs follow ==="
+  cat log
+}
+trap error ERR
+
+dir=$(dirname "$0")
+name=cf-remote-debian-test-host
+
+docker stop "$name" || true
+docker rm "$name" || true
+docker build -t "$name" "$dir" >log 2>&1
+docker run -d -p 8822:22 --name "$name" "$name" >>log 2>&1
+ip_addr=$(hostname -i)
+ssh -o StrictHostKeyChecking=no -p 8822 root@"$ip_addr" hostname >>log 2>&1
+cf-remote --log-level DEBUG install --clients root@"$ip_addr":8822 2>&1 | tee -a log
+ssh -o StrictHostKeyChecking=no -p 8822 root@"$ip_addr" cf-agent -V >>log 2>&1

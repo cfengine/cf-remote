b9a96431f9b3a0a442f654beb72a093571c0ea7a
Craig Comstock <craig.comstock@northern.tech>
2025-02-14 15:25:07 -0600

    remove github debug bits specific to python 3.5.10

--- tests/shell/001-docker-install-test.sh
+++ tests/shell/001-docker-install-test.sh
@@ -6,9 +6,6 @@ error () {
 }
 trap error ERR
 
-# debug github environment
-ls -l /opt/hostedtoolcache/Python/3.5.10/x64/lib/python3.5/site-packages/cf_remote
-stat /opt/hostedtoolcache/Python/3.5.10/x64/lib/python3.5/site-packages/cf_remote/nt-discovery.sh
 dir=$(dirname "$0")
 name=cf-remote-debian-test-host
 

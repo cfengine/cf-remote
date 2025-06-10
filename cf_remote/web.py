4fdf8fcc9a439677984710823dc5fdbc9e873d15
luterlassus <vegard.otterlei@northern.tech>
2025-06-10 16:49:00 +0200

    Updated downloader to download into temporary files rather than memory. Seperated lockfiles to other tempfiles. Wrong checksum on pre-existing file now triggers warning, deletion of pre-existing file and re-download rather than raising of error.

--- cf_remote/web.py
+++ cf_remote/web.py
@@ -3,13 +3,17 @@ import fcntl
 import re
 import urllib.request
 import json
+import tempfile
 from collections import OrderedDict
 from cf_remote.utils import (
     is_different_checksum,
+    file_has_different_checksum,
     user_error,
     write_json,
     mkdir,
     parse_json,
+    copy_file,
+    delete_file,
 )
 from cf_remote import log
 from cf_remote.paths import cf_remote_dir, cf_remote_packages_dir
@@ -39,45 +43,45 @@ def download_package(url, path=None, checksum=None):
             "Invalid checksum or unsupported checksum algorithm: '%s'" % checksum
         )
 
+    filename = os.path.basename(url)
+
     if not path:
-        filename = os.path.basename(url)
         directory = cf_remote_packages_dir()
         mkdir(directory)
         path = os.path.join(directory, filename)
 
-    # Use "ab" to prevent truncation of the file in case it is already being
-    # downloaded by a different thread.
-    with open(path, "ab+") as f:
-        # Get an exclusive lock. If the file size is != 0 then it's already
-        # downloaded, otherwise we download.
-        fcntl.flock(f.fileno(), fcntl.LOCK_EX)
-        st = os.stat(path)
-        if st.st_size != 0:
+    tempfolder = os.path.join(tempfile.gettempdir(), "cf-engine")
+    mkdir(tempfolder)
+    # Check if other process is handeling this - lockfile
+    lockfile = os.path.join(tempfolder, f"{filename}.lock")
+    with open(lockfile, "w") as lf:
+        fcntl.flock(lf.fileno(), fcntl.LOCK_EX)
+        # Check if file already exists, if so check the hash if ok - return
+        if os.path.exists(path):
+            # File already exist, check the hash
             print("Package '{}' already downloaded".format(path))
+            if file_has_different_checksum(path, checksum):              
+                delete_file(path)
+                log.warning("Pre-existing file did not match existing checksum. File is now deleted.")
+            else:
+                # Checksum was correct, skip download
+                return path
 
-            f.seek(0)
-            content = f.read()
-            if checksum and is_different_checksum(checksum, content):
-                raise ChecksumError(
-                    "Downloaded file '{}' does not match expected checksum '{}'. Please delete the file.".format(
-                        filename, checksum
-                    )
+        # File does not exist, download it
+        print("Downloading package: '{}'".format(path))
+        tempfilename, header = urllib.request.urlretrieve(url)
+        if file_has_different_checksum(tempfilename, checksum):
+            urllib.request.urlcleanup()
+            raise ChecksumError(
+                "Downloaded file '{}' does not match expected checksum '{}'.".format(
+                    filename, checksum
                 )
+            )
 
-        else:
-            print("Downloading package: '{}'".format(path))
-
-            answer = urllib.request.urlopen(url).read()
-            if checksum and is_different_checksum(checksum, answer):
-                raise ChecksumError(
-                    "Downloaded file '{}' does not match expected checksum '{}'. Please delete the file.".format(
-                        filename, checksum
-                    )
-                )
-
-            f.write(answer)
-            f.flush()
-
-        fcntl.flock(f.fileno(), fcntl.LOCK_UN)
-
+        # Copy over the tempfile and remove it
+        copy_file(tempfilename, path)
+        urllib.request.urlcleanup()
+        # Unlock the lockfile
+        fcntl.flock(lf.fileno(), fcntl.LOCK_UN)
+    
     return path
